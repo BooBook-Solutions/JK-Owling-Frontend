f305303b6534da9098193431e12e994720f85b6e
wamuumu <matteo68.mc@gmail.com>
2023-12-29 20:44:31 +0100

    Fixed search: excluded urls from search fields

--- src/Components/Common/SearchBar.jsx
+++ src/Components/Common/SearchBar.jsx
@@ -1,22 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const SearchBar = ({ items, setItems, placeholder }) => {
 
-    const [searchTerm, setSearchTerm] = useState('');
-    
-    useEffect(() => {
-        if(!searchTerm) {
-            setItems(items)
-            console.log(items);
-        }
-    }, [searchTerm])
+    // Function to check if a string is a link
+    function isLink(value) {
+      const linkRegex = /^(http|https):\/\/[\w\-]+(\.[\w\-]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?$/;
+      return linkRegex.test(value);
+    }
 
     const handleSearch = (query) => {
-        setSearchTerm(query);
-        const filteredItems = items.filter((item) =>
-            Object.values(item).some((value) => String(value).toLowerCase().includes(query.toLowerCase()))
-        );
-        setItems(filteredItems);
+      console.log(query);
+      const filteredItems = items.filter((item) =>
+          Object.values(item).some((value) => !isLink(value) && String(value).toLowerCase().includes(query.toLowerCase()))
+      );
+      console.log(filteredItems);
+      if(query) setItems(filteredItems);
+      else setItems(items);
     };
 
   return (
